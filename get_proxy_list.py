7247532d792c9c156067d06fb0fb07ed1ed583a1
Sbtha <shisastore@protonmail.com>
2021-10-08 01:21:54 +0200

    Added get_proxy_list.py that searches list of public proxies and return a set of ready to use servers.

--- get_proxy_list.py
+++ get_proxy_list.py
@@ -1,43 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
-from requests.api import head
 import re
+
 
 # !! Proxy Test !!
 # test_url = 'https://httpbin.org/ip'
-
-
-# Normal Function.
-url = 'https://free-proxy-list.net/'
-
-
-proxies = proxies = {
-    "http": 'http://78.92.231.44:55443',
-    "https": 'http://78.92.231.44:55443',
-    "http": 'http://54.93.108.168:9300',
-    "https": 'http://54.93.108.168:9300',
-}
-
-
-headers = {
-    # 'X-Requested-With': 'XMLHttpRequest',
-    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36",
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept': 'image/webp,*/*',
-    # 'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-CH': 'UA, Platform',
-}
-
 # !! This checks if request went from proxy !!
 # test_r = requests.get(test_url, proxies=proxies)
 # print(test_r.json())
 
-r = requests.get(url, headers=headers)
-soup = BeautifulSoup(r.text, 'lxml')
-# print(soup.prettify())
 
-tr = soup.select('tbody > tr > td')
-# print(tr)
-for td in tr:
-    print(td)
+url = 'https://free-proxy-list.net/'
+
+
+def get_proxies(url):
+
+    proxies = set()
+
+    ip_pattern = re.compile(r'[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+')
+    port_pattern = re.compile(r'\B[0-9]{2,5}\B')
+
+    headers = {
+        # 'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36",
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept': 'image/webp,*/*',
+        # 'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-CH': 'UA, Platform',
+    }
+
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+    # print(soup.prettify())
+
+    tr = soup.select('.table.table-striped.table-bordered > tbody > tr')
+    # print(tr)
+
+    for td in tr:
+        # print(td)
+
+        ip_address = ip_pattern.findall(str(td))
+        port_number = port_pattern.findall(str(td))
+        proxy = ':'.join(ip_address+port_number)
+        # print(proxy)
+        proxies.add(proxy)
+
+    return proxies
+
+
+print(get_proxies(url))
